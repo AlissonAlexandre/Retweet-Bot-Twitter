de239592005de9e50c8c74bc792fe1d6df5ed6ef
AlissonAlexandre <botelho.alissonalexandre@gmail.com>
2022-10-09 23:37:40 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -33,5 +33,6 @@ class ClassStream(tweepy.StreamingClient):
 stream = ClassStream(bearer_token=BEARER_TOKEN)
 regra = tweepy.StreamRule("couve -is:retweet -is:quote -is:reply")
 stream.add_rules(regra, dry_run=False)
+print(stream.get_rules())
 print("Stream rodando!")
 stream.filter(tweet_fields=['author_id', 'edit_history_tweet_ids', 'id', 'text'])
